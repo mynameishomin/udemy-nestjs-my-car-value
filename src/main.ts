5456508671069f9c61a1bf315069886fdc0e9fcf
mynameishomin <mynameishomin@gmail.com>
2024-08-02 17:47:26 +0900

    엔드투엔드 테스트 파일 작성

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['dasddhqowfnvo1238eyh'],
-    }),
-  );
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000);
 }
 bootstrap();
